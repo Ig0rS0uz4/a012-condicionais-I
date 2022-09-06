5f81d14d6f9bc0eb89f90a3406e97c27b1e7621f
igorazuos <azuosigor@gmail.com>
2022-09-06 14:54:51 -0300

    atualização final a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,18 +1,18 @@
-let idade = 24
-let cursouEnsinoMedio = true
-let CursandoUmaFaculdade = false
+let idade = prompt("dia sua idade")
+let cursouEnsinoMedio = prompt("voce ja completou o ensino medio?")
+let CursandoUmaFaculdade = prompt("voce esta cursando uma faculdade?")
 
 if(idade >= 18){
     console.log("voce é maior de idade")
 }else{
 	console.log("voce é menor de idade")
 }
-if (cursouEnsinoMedio){
+if (cursouEnsinoMedio === "sim"){
     console.log("Ensino médio completo")
 }else{
     console.log("Ensino médio incompleto")
 }
-if(CursandoUmaFaculdade){
+if(CursandoUmaFaculdade === "sim"){
     console.log("Voce esta cursando uma faculdade")
 }else{
     console.log("voce não esta cursando uma faculdade")
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,22 @@
-let nacionalidade //= adicione o prompt aqui
+const pergunta = prompt("digite aqui a sua nacionalidade").toLowerCase()
+const nacionalidade = {
+    pais1: "brasileira",
+    pais2: "argentina",
+    pais3: "uruguaia",
+    pais4: "chilena",
+    pais5: "colombiana"
+}
+
+if(nacionalidade.pais1 === pergunta){
+    console.log("nacionalidade encontrada")
+}else if (nacionalidade.pais2 === pergunta){
+    console.log("nacionalidade encontrada")
+}else if (nacionalidade.pais3 === pergunta){
+    console.log("nacionalidade encontrada")
+}else if (nacionalidade.pais4 === pergunta){
+    console.log("nacionalidade encontrada")
+}else if (nacionalidade.pais5 === pergunta){
+    console.log("nacionalidade encontrada")
+}else{
+    console.log("nacionalidade não encontrada")
+}
