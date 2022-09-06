4ef47a46daefd99958c736e08bf06e00873ab8bc
igorazuos <azuosigor@gmail.com>
2022-09-06 12:36:38 -0300

    atualização exercicio2 a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,19 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 24
+let cursouEnsinoMedio = true
+let CursandoUmaFaculdade = false
 
-if(){
-
+if(idade >= 18){
+    console.log("voce é maior de idade")
 }else{
-	
+	console.log("voce é menor de idade")
+}
+if (cursouEnsinoMedio){
+    console.log("Ensino médio completo")
+}else{
+    console.log("Ensino médio incompleto")
+}
+if(CursandoUmaFaculdade){
+    console.log("Voce esta cursando uma faculdade")
+}else{
+    console.log("voce não esta cursando uma faculdade")
 }
